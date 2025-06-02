f33769a36d1a517bc358ae27ad0823d5571ad0cf
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-06-02 17:45:21 +0200

    add: snack 4

--- snacks.test.js
+++ snacks.test.js
@@ -1,7 +1,8 @@
 const {
     getInitials,
     createSlug,
-    average
+    average,
+    createNewSlug
 } = require("./snacks.js");
 
 const numbers = [1, 3, 7, 9, 5];
@@ -20,3 +21,8 @@ test("La funzione createSlug restituisce una stringa in lowercase.", () => {
 test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
     expect(average(numbers)).toBe(5);
 });
+
+// 4
+test("La funzione createSlug sostituisce gli spazi con -.", () => {
+    expect(createNewSlug("Frase dove verranno sostituiti gli spazi con i trattini")).toBe("Frase-dove-verranno-sostituiti-gli-spazi-con-i-trattini")
+})
