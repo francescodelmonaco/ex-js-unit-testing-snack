e40ccfc810f7fe5d5b257842430e234ce95b634f
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-06-03 11:34:55 +0200

    add: snack 7

--- snacks.js
+++ snacks.js
@@ -4,7 +4,7 @@ function getInitials(fullName) {
     return `${name.charAt(0).toUpperCase()} ${surname.charAt(0).toUpperCase()}`;
 };
 
-// 2 // 4
+// 2 // 4 // 6
 function createSlug(string) {
     if (!string) {
         throw new Error("Titolo non valido");
@@ -27,9 +27,29 @@ function isPalindrome(string) {
     return lowercaseString === revString;
 };
 
+// 7
+function findPostById(array, id) {
+    if (isNaN(id)) {
+        throw new Error(`${id} non è un id`);
+    };
+
+    array.forEach(element => {
+        if (
+            element.id === undefined ||
+            element.title === undefined ||
+            element.slug === undefined
+        ) {
+            throw new Error("L'array non è nel formato corretto");
+        };
+    });
+
+    return array.find(a => a.id === id) || null;
+};
+
 module.exports = {
     getInitials,
     createSlug,
     average,
-    isPalindrome
+    isPalindrome,
+    findPostById
 };
--- snacks.test.js
+++ snacks.test.js
@@ -2,10 +2,39 @@ const {
     getInitials,
     createSlug,
     average,
-    isPalindrome
+    isPalindrome,
+    findPostById
 } = require("./snacks.js");
 
 const numbers = [1, 3, 7, 9, 5];
+
+const posts = [
+    {
+        id: 1,
+        title: "Introduzione a JavaScript",
+        slug: "introduzione-a-javascript"
+    },
+    {
+        id: 2,
+        title: "Guida a React",
+        slug: "guida-a-react"
+    },
+    {
+        id: 3,
+        title: "Come funziona il DOM",
+        slug: "come-funziona-il-dom"
+    },
+    {
+        id: 4,
+        title: "Async/Await in JavaScript",
+        slug: "async-await-in-javascript"
+    },
+    {
+        id: 5,
+        title: "CSS Grid vs Flexbox",
+        slug: "css-grid-vs-flexbox"
+    }
+];
 
 // 1
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
@@ -37,4 +66,12 @@ test("La funzione isPalindrome verifica se una stringa è un palindromo.", () =>
 test("La funzione createSlug lancia un errore se il titolo è vuoto o non valido.", () => {
     expect(() => createSlug("")).toThrow("Titolo non valido");
     expect(() => createSlug(null)).toThrow("Titolo non valido");
+});
+
+// 7
+test("La funzione findPostById restituisce il post corretto dato l’array di post e l’id", () => {
+    expect(findPostById(posts, 2)).toEqual({ id: 2, title: "Guida a React", slug: "guida-a-react" });
+    expect(findPostById(posts, 6)).toBe(null);
+    expect(() => findPostById(posts, "text")).toThrow("text non è un id");
+    expect(() => findPostById([10, 20], 2)).toThrow("L'array non è nel formato corretto");
 });
